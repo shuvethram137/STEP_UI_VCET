531eef294626aae0561339d7ebc05aae291ac6cd
shuvethram137 <78020721+shuvethram137@users.noreply.github.com>
2023-11-19 14:44:36 +0530

    added resouces

--- src/pages/RestaurantList.js
+++ src/pages/RestaurantList.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import HeaderBar from "../components/HeaderBar";
+import { RestaurantService } from "../services/RestaurantService";
+import "../styles/restaurantListPageStyles.css";
+ 
 const RestaurantList = () => {
-    return <h1>restaurant list page</h1>
+ 
+  //TODO: Initialize resturant state.
+ 
+/**
+* Fetch the list of restaurants from RestaurantService and updates the component state.
+* Sets the state to display loading indicator during data fetch from service.
+*/
+  async function fetchRestaurantList() {
+    // setRestaurantState({...restaurantState, isLoading:true})
+    // data from API was strored in restaurantList.
+    let restaurantsList = await RestaurantService.getRestaurantsList();
+    
+    // TOOD: set state accordingly.
+    
+  }
+ 
+  //TODO: API CALL
+ 
+ 
+ 
+  return (
+    <div className="restaurant-list-container">
+      <HeaderBar />
+      <div className="restaurant-list-wrapper child">
+        {/* TODO: Iterate the restaurant */}
+      </div>
+ 
+    </div>
+  );
 }
-
+ 
 export default RestaurantList;
+ 
